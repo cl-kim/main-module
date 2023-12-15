cc1b5401484273acfba48ba8fa8605769ff86cab
cl-kim <nias0327@naver.com>
2023-12-15 09:42:39 +0900

    project init

---
+++ src/test/java/hello/mainmodule/MainModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.mainmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
